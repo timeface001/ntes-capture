e86de63fc1522a0d45f178c2a91fae38b8d6aa6b
冯松 <hardly.a.challenge.for.me@gmail.com>
2017-07-12 20:59:43 +0800

    ss

---
+++ src/main/java/com/fs/dreams/CaptureUtils.java
@@ -0,0 +1,18 @@
+package com.fs.dreams;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author fengsong
+ * @description:一句话描述下类的功能
+ * @time 2017-07-12 20:55
+ **/
+public class CaptureUtils {
+
+    List<Artist> searchArtist(String artistName) {
+        List<Artist> resultList = new LinkedList<Artist>();
+
+        return resultList;
+    }
+}
