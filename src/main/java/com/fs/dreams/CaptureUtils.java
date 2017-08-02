99d169d1fac9ff1c6229939ca1e26ddc7144267c
冯松 <hardly.a.challenge.for.me@gmail.com>
2017-08-02 11:07:17 +0800

    完成 歌曲详情、专辑、歌手搜索接口

--- src/main/java/com/fs/dreams/CaptureUtils.java
+++ src/main/java/com/fs/dreams/CaptureUtils.java
@@ -3,6 +3,7 @@ package com.fs.dreams;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.fs.dreams.dto.FPageResponse;
 import com.fs.dreams.utils.*;
 import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.Request;
@@ -10,7 +11,10 @@ import com.squareup.okhttp.Response;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author fengsong
@@ -75,19 +79,39 @@ public class CaptureUtils {
         return FStringUtil.EMPTY;
     }
 
-    public static List<Artist> searchArtist(String artistName, int pageNum, int pageSize) {
-        List<Artist> resultList = new LinkedList<Artist>();
+    public static FPageResponse<Artist> searchArtist(String artistName, int pageNum, int pageSize) {
+        List<Artist> list = new ArrayList<Artist>();
+        int artistCount = 0;
         try {
             String response = OkHttpUtil.post(NtesApi.SEARCH_URL, GeneralUtils.generateMap(
                     "s", artistName, "offset", getOffset(pageNum, pageSize), "limit", pageSize, "type", NtesApi.SEARCH_TYPE_ARTIST));
             if (FStringUtil.isNoneBlank(response)) {
-                System.out.println(JSON.toJSONString(JSON.parseObject(response), true));
+                artistCount = JSON.parseObject(response).getJSONObject("result").getInteger("artistCount");
+                if (artistCount > 0) {
+                    JSONArray jsonArray = JSON.parseObject(response).getJSONObject("result").getJSONArray("artists");
+                    for (int i = 0; i < jsonArray.size(); i++) {
+                        JSONObject jb = jsonArray.getJSONObject(i);
+
+                        Artist artist = new Artist();
+                        artist.setSrcId(jb.getLong("id"));
+                        artist.setAlbumSize(jb.getInteger("albumSize"));
+                        artist.setAlias(FJSONUtil.joinJSONArray(jb.getJSONArray("alias"), ","));
+                        artist.setAvatar(jb.getString("picUrl"));
+                        //artist.setMusicSize(jb.getInteger("musicSize"));
+                        artist.setCreateTime(new Date());
+                        artist.setName(jb.getString("name"));
+
+                        list.add(artist);
+                    }
+
+                }
+
 
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return resultList;
+        return new FPageResponse<Artist>(list, pageNum, pageSize, artistCount);
     }
 
     public static List<Artist> getTopArtist(int pageNum, int pageSize) {
@@ -146,31 +170,86 @@ public class CaptureUtils {
         return songs;
     }
 
-    public static List<Song> searchSongsByAlbumName(String albumName, Long albumId, int pageNum, int pageSize) {
+    public static List<Song> searchSongsByAlbumName(String albumName, Long albumId) {
         List<Song> songs = new ArrayList<Song>();
         try {
-            String response = OkHttpUtil.post(NtesApi.ARTIST_SONG, GeneralUtil.generateMap("s", albumName, "offset", getOffset(pageNum, pageSize), "limit", pageSize, "type", NtesApi.SEARCH_TYPE_SONG));
-            if (StringUtils.isNotBlank(response)) {
-                JSONObject jsonObject = JSON.parseObject(response);
-                if (jsonObject.getInteger("songCount") != 0) {
+            int pageNum = 1, pageSize = 10;
+            int songCount = 0;
+            do {
+                String response = OkHttpUtil.post(NtesApi.ARTIST_SONG, GeneralUtil.generateMap("s", albumName, "offset", getOffset(pageNum, pageSize), "limit", pageSize, "type", NtesApi.SEARCH_TYPE_SONG));
+                if (StringUtils.isNotBlank(response)) {
+                    JSONObject jsonObject = JSON.parseObject(response).getJSONObject("result");
+                    songCount = jsonObject.getInteger("songCount");
                     JSONArray jsonArray = jsonObject.getJSONArray("songs");
 
-                    for (int i = 0; i < jsonArray.size(); i++) {
-                        Song song = new Song();
-                        JSONObject jb = jsonArray.getJSONObject(i);
+                    if (!jsonArray.isEmpty()) {
+                        for (int i = 0; i < jsonArray.size(); i++) {
+                            JSONObject jb = jsonArray.getJSONObject(i);
 
-                        //验证albumID
-                        if (albumId.equals(jb.getJSONObject("al").getLong("id"))) {
-                            songs.add(songDetail(jb.getLong("id")));
+                            //验证albumID
+                            if (albumId.equals(jb.getJSONObject("al").getLong("id"))) {
+                                songs.add(songDetail(jb.getLong("id")));
+                            }
                         }
+                        pageNum++;
                     }
+                } else {
+                    songCount = 0;
                 }
-            }
+            } while (songCount > (pageNum - 1) * pageSize);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         return songs;
+    }
+
+
+    public static List<Album> searchAlbumsByArtist(String artistName, Long artistId) {
+        List<Album> albums = new ArrayList<Album>();
+        try {
+            int pageNum = 1, pageSize = 20;
+            int albumCount = 0;
+            do {
+                String response = OkHttpUtil.post(NtesApi.SEARCH_URL, GeneralUtil.generateMap("s", artistName, "offset", getOffset(pageNum, pageSize), "limit", pageSize, "type", NtesApi.SEARCH_TYPE_ALBUM));
+                if (StringUtils.isNotBlank(response)) {
+                    JSONObject jsonObject = JSON.parseObject(response);
+                    albumCount = jsonObject.getJSONObject("result").getInteger("albumCount");
+                    JSONArray albumsArray = jsonObject.getJSONObject("result").getJSONArray("albums");
+                    if (Objects.nonNull(albumsArray) && !albumsArray.isEmpty()) {
+                        for (int i = 0; i < albumsArray.size(); i++) {
+                            JSONObject albumObject = albumsArray.getJSONObject(i);
+                            if ((albumObject.getJSONObject("artist").getLong("id")).equals(artistId)) {
+                                Album album = new Album();
+                                album.setCommentThreadId(albumObject.getString("commentThreadId"));
+                                album.setArtistSrcId(albumObject.getJSONObject("artist").getLong("id"));
+                                album.setCompany(albumObject.getString("company"));
+                                album.setDescription(albumObject.getString("description"));
+                                album.setName(albumObject.getString("name"));
+                                album.setPicUrl(albumObject.getString("picUrl"));
+                                album.setPublishTime(albumObject.getLong("publishTime"));
+                                album.setSize(albumObject.getByte("size"));
+                                album.setSrcId(albumObject.getLong("id"));
+                                album.setType(albumObject.getString("type"));
+                                album.setCreateTime(new Date());
+
+                                albums.add(album);
+                            }
+                        }
+
+                        pageNum++;
+                    }
+
+                } else {
+                    albumCount = 0;
+                }
+            } while (albumCount > (pageNum - 1) * pageSize);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return albums;
     }
 
 
@@ -181,6 +260,10 @@ public class CaptureUtils {
     public static void main(String[] args) {
         //System.out.println(JSON.toJSONString(getTopArtist(1,100),true));;
         //searchSongs("周杰伦", 1, 10);
-        System.out.println(JSON.toJSONString(songDetail(418602084L)));
+       /* System.out.println(JSON.toJSONString(searchSongsByAlbumName("十一月的萧邦", 18896L)));
+        ;
+        System.out.println(JSON.toJSONString(searchAlbumsByArtist("周杰伦", 6452L)));
+        ;*/
+        System.out.println(JSON.toJSONString(searchArtist("周杰", 1, 10)));
     }
 }
